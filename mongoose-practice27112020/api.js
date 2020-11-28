5afcbc189216493e049e8f06a7e31b3f82cd7474
basselhilf <basselhilf@gmail.com>
2020-11-28 02:03:25 +0200

    mongoose-practice

---
+++ mongoose-practice27112020/api.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+
+const Person = require('../models/Person')
+
+router.get('/people', function (req, res) {
+    Person.find({}, function (err, people) {
+        res.send(people)
+    })
+})
+
+router.post('/person', function(req, res) {
+    let person = req.body
+    let p1 = new Person({
+        firstName: person.firstName,
+        lastName: person.lastName,
+        age: person.age
+    })
+    
+    p1.save()
+    res.end()
+})
+
+router.put('/person/:id', function(req, res) {
+    let id = req.params.id
+    Person.findByIdAndUpdate(id, {age: 80}, function(err, person) {
+        res.end()
+    })
+})
+
+router.delete('/apocalypse', function(req, res) {
+	
+
+    Person.find({}, function(err, people) {
+        people.forEach(p => p.remove())
+    })
+
+
+    res.end()
+})
+
+module.exports = router
